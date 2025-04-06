f69c62548c0f404f1cb2deb6630490db07d2c042
KIRIt141 <uffff90124@gmail.com>
2025-04-06 16:17:20 +0300

    first commit

---
+++ db/sqdb3.py
@@ -0,0 +1,56 @@
+import sqlite3 as sq
+
+
+def init_db():
+    conn = sq.connect('my_database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id_tg INTEGER UNIQUE,
+        username TEXT NOT NULL,
+        sleep_status TEXT NOT NULL DEFAULT 'unknown'
+    )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def add_user(user_id: int, username: str):
+    conn = sq.connect('my_database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT OR IGNORE INTO Users (user_id_tg, username)
+    VALUES (?, ?)
+    ''', (user_id, username))
+
+    conn.commit()
+    conn.close()
+
+def sleep_yes(user_id: int, username: str, sleep_status: str):
+    conn = sq.connect('my_database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT OR REPLACE INTO Users (user_id_tg, username, sleep_status)
+    VALUES (?, ?, ?)
+    ''', (user_id, username, sleep_status))
+
+    conn.commit()
+    conn.close()
+
+def sleep_no(user_id: int, username: str, sleep_status: str):
+    conn = sq.connect('my_database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT OR REPLACE INTO Users (user_id_tg, username, sleep_status)
+    VALUES (?, ?, ?)
+    ''', (user_id, username, sleep_status))
+
+    conn.commit()
+    conn.close()
+
+def all_users():
+    with sq.connect('my_database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT user_id_tg, username, sleep_status FROM Users')
+        return cursor.fetchall()     
---
+++ main.py
@@ -0,0 +1,66 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters.command import Command
+from aiogram.types import Message, CallbackQuery 
+
+import app.keyboar as kb
+from db.sqdb3 import init_db, add_user, sleep_yes, sleep_no, all_users
+from dotenv import load_dotenv
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+logging.basicConfig(level=logging.INFO)
+dp = Dispatcher()
+init_db()
+
+
+@dp.message(Command('start'))
+async def cmd_start(message:Message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    add_user(user_id, username)
+    await message.answer(f'Hello {message.from_user.full_name}. This bot is designed to monitor your friends sleep.', reply_markup=await kb.inline_key())
+
+
+@dp.callback_query(F.data == 'Sleep')
+async def Sleep(callback:CallbackQuery):
+    user_id = callback.from_user.id
+    username = callback.from_user.username
+    sleep_status = 'Sleep'
+    sleep_yes(user_id, username, sleep_status)
+    await callback.answer()
+    await callback.message.answer('😵 Sleep')
+
+@dp.callback_query(F.data == 'Not_Sleeping')
+async def Not_Sleep(callback:CallbackQuery):
+    user_id = callback.from_user.id
+    username = callback.from_user.username
+    sleep_status = 'Not_Sleeping'
+    sleep_no(user_id, username, sleep_status)
+    await callback.answer()
+    await callback.message.answer('👁 Not sleeping')
+
+@dp.callback_query(F.data == 'Check your friends')
+async def Check_friends(callback:CallbackQuery):
+    users = all_users()
+    if not users:
+        response = 'No user data'
+    else:
+        response = 'Friends statuses:\n'
+        for user in users:
+            status = '👁 Not sleeping' if user[2] == 'Not_Sleeping' else '😵 Sleep'
+            response += f'{user[1]}: {status}\n'
+    await callback.answer()
+    await callback.message.answer(response)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    print('Бот польностью готов к работе')
+    asyncio.run(main())
+
